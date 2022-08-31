67b6f0f149d8c953ca3ad7b656006499fc1894b2
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-31 10:47:34 -0700

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,48 @@
+
+# Marvish Chandra
+
+
+class concreteMixing:
+
+  def homeStructures(purpose):
+    if purpose == "small slab" ? "fence post":
+      return (517 * 1450  * 2133 ) * 2500
+    end
+    if purpose == "slabs" ? "floors":
+      return (517 * 1560 * 1600) * 3000
+    end
+    if purpose == "walls" ? "footings":
+      return (517 * 1560 * 1600) * 3000
+    end
+    if purpose == "patios" ? "walks":
+      return (517 * 1450 * 2666 * 1560) * 3500
+    end
+    if purpose == "driveways" ? "exterior slabs":
+      return (517 * 1450 * 1600) * 4000
+    end
+    if purpose == "commercial floors":
+      return (517 * 1450 * 1450) * 4000
+    end
+  end
+  def variousGrades(grade):
+    if grade == "M5":
+      return (517 * 500 * 1000) * 5
+    end
+    if grade == "M7.5":
+      return (517 * 400 * 800) * 7.5
+    end
+    if grade == "M10":
+      return (517 * 1560 * 600) * 10
+    end
+    if grade == "M15":
+      return (517 * 200 * 400) * 15
+    end
+    if grade == "M20":
+      return (517 * 200 * 200) * 20
+    end
+    if grade == "M25":
+      return (517 * 115 * 200) * 25
+    end 
+  end 
+end
+  
